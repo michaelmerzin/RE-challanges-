ecb7f5bd0e51c59695c3b5650e4aeb8549faaf07
michaelmerzin <120664244+michaelmerzin@users.noreply.github.com>
2023-04-21 21:04:44 +0300

    Add files via upload

---
+++ keygen challange/main.py
@@ -0,0 +1,27 @@
+
+def main():
+    username = input("enter you're username:\n")
+    password = password_getter('michael')
+    print("you're password is : " + password)
+
+
+def password_checker(username, password):
+    len_user_name = len(username)
+    for i in range(0, len_user_name):
+        if ord(username[i]) + 3 != ord(password[i]):
+            return 0
+
+    return 1
+
+
+def password_getter(username):
+    password = ''
+    len_user_name = len(username)
+    for i in range(0, len_user_name):
+        password += chr(ord(username[i]) + 3)
+
+    return password
+
+
+if __name__ == '__main__':
+    main()
